2d0762e912063a38363d7e5e3e4841b5235f617d
dev-afhasani <dev.afhasani@gmail.com>
2024-07-13 10:50:51 +0700

    Create route get data by id

--- src/index.mjs
+++ src/index.mjs
@@ -4,17 +4,29 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const users = [
+  { id: 1, username: "abdillah", displayName: "Abdillah" },
+  { id: 2, username: "fakhri", displayName: "Fakhri" },
+  { id: 3, username: "hasani", displayName: "Hasani" }
+];
+
 app.get('/', (req, res) => {
   res.status(201).send({ message: 'Hi I am Sani!' });
 });
 
 app.get('/api/users', (req, res) => {
-  res.status(201).send([{
-    id: 1,
-    username: "Fakhri",
-    displayName: "Fakh"
-  }]);
+  res.status(201).send(users);
 });
+
+app.get('/api/users/:id', (req, res) => {
+  const parsedId = parseInt(req.params.id)
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "Bad Request. Invalid id" })
+
+  const findUser = users.find((user) => user.id === parsedId)
+  if (!findUser) return res.sendStatus(404)
+  return res.send(findUser)
+
+})
 
 app.get('/api/products', (req, res) => {
   res.status(201).send([{
