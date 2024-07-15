31d404731fcb47891f11d2a97aad97b0f700ffe6
dev-afhasani <dev.afhasani@gmail.com>
2024-07-15 13:27:40 +0700

    Move data to own files

---
+++ src/utils/constants.mjs
@@ -0,0 +1,16 @@
+export const posts = [
+  { id: 1, author: "abdillah", title: "A" },
+  { id: 2, author: "fakhri", title: "B" },
+  { id: 3, author: "hasani", title: "C" }
+];
+
+export const users = [
+  { id: 1, username: "abdillah", displayName: "Abdillah" },
+  { id: 2, username: "fakhri", displayName: "Fakhri" },
+  { id: 3, username: "hasani", displayName: "Hasani" }
+];
+
+export const products = [
+  { id: 1, name: "Laptop", price: 8000000 },
+  { id: 1, name: "Table", price: 1000000 }
+];
