a9044995cadd872ccd298a44463b4372d30690c0
dev-afhasani <dev.afhasani@gmail.com>
2024-07-15 21:00:41 +0700

    Create cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -7,11 +8,14 @@ const PORT = process.env.PORT || 4000;
 // Set middleware in order to allow JSON
 app.use(express.json());
 
+app.use(cookieParser('cookies1'))
+
 // Import all router from src/index
 app.use(routes);
 
 // Get root address
 app.get('/', (req, res) => {
+  res.cookie("hello", "world", { maxAge: 10000, signed: true });
   res.status(200).send("<h1> Hi! Welcome to my website </h1>");
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -4,7 +4,12 @@ import { products } from "../utils/constants.mjs";
 const router = Router();
 
 router.get('/api/products', (req, res) => {
-  res.status(200).send(products);
+  // console.log(req.headers.cookie)
+  console.log(req.cookies)
+  console.log(req.signedCookies.hello)
+  if (req.signedCookies.hello && req.signedCookies.hello === 'world')
+    return res.status(200).send(products);
+  return res.status(403).send({ msg: "not authorized" })
 });
 
 export default router;
